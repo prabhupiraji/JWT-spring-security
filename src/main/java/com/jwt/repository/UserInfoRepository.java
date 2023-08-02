3f43fad3e18a9de6af8c2c85ebccc05143d0e3d1
prabhu <prabhupiraji1998@gmail.com>
2023-08-02 12:28:38 +0530

    localmaster

---
+++ src/main/java/com/jwt/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package com.jwt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jwt.entity.UserInfo;
+
+import java.util.Optional;
+
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByName(String username);
+
+}
